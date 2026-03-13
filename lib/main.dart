c84bd14c07cdea5eeed064238bbf828fb13893c9
solomonbestz <kingsolodawarrior@gmail.com>
2026-03-14 00:32:26 +0100

    fix: Updated the main to the new go green class

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,9 @@
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'package:flame_docs/go_green.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(GameWidget(game: FlameGame(world: MyWorld())));
-}
-
-class Player extends SpriteComponent with TapCallbacks {
-  Player({super.position})
-    : super(size: Vector2.all(200), anchor: Anchor.center);
-
-  @override
-  Future<void> onLoad() async {
-    sprite = await Sprite.load('JUMP.png');
-    if (sprite != null) {
-      print('Sprite loaded successfully');
-    } else {
-      print('Failed to load sprite');
-    }
-  }
-
-  @override
-  void onTapUp(TapUpEvent info) {
-    size += Vector2.all(50);
-  }
-}
-
-class MyWorld extends World {
-  @override
-  Future<void> onLoad() async {
-    add(Player(position: Vector2(0, 0)));
-  }
+  runApp(GameWidget(game: GoGreen()));
 }
