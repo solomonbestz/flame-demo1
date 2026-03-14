63b1eb14f411f8efa1f07a7b8dfc115853943d33
solomonbestz <kingsolodawarrior@gmail.com>
2026-03-14 01:14:30 +0100

    feat: Updated the go green file

--- lib/go_green.dart
+++ lib/go_green.dart
@@ -1,22 +1,38 @@
 import 'dart:ui';
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame_docs/constants.dart';
 import 'package:flame_docs/player.dart';
 
 class GoGreen extends FlameGame {
-  GoGreen({super.children});
+  GoGreen() 
+    : super(
+      camera: CameraComponent.withFixedResolution(
+        width: gameWidth,
+        height: gameHeight,
+      )
+    );
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
 
     world.add(Player(
-      position: Vector2(0, 0), 
-      radius: 50));
+        position: Vector2(-(gameWidth / 4), 0), 
+        radius: gameWidth / 4
+      )
+    );
+
+    world.add(Player(
+      position: Vector2(0, 100), 
+      radius: 20,
+      color: Color.fromARGB(255, 16, 29, 211)
+    ));
   }
 
   @override
   Color backgroundColor() {
-    return Color.fromARGB(255, 16, 211, 20);
+    return Color.fromARGB(255, 104, 211, 16);
   }
 }
