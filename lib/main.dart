9e022da1fa8b8c5c646e3f5902be55f4afeeef80
solomonbestz <kingsolodawarrior@gmail.com>
2026-03-14 01:13:01 +0100

    feat: updated the main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:flame/events.dart';
-import 'package:flame/game.dart';
-import 'package:flame/components.dart';
-import 'package:flame_docs/go_green.dart';
+import 'package:flame/flame.dart';
+import 'package:flame_docs/game_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(GameWidget(game: GoGreen()));
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  runApp(const GameApp());
 }
